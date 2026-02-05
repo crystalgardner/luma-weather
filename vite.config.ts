c2310b3002f01e646cc2744674467b9309f100ea
Crystal Reyes <crystalreyesdev@gmail.com>
2026-02-05 02:22:16 -0500

    Add path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: "/luma-weather/",
   server: {
     host: "::",
     port: 8080,
